ab039326e70c3f694add6e1abd0be2bc0c3be45e
Amarire Dev <108834223+Said-Amarire@users.noreply.github.com>
2025-11-09 20:39:03 +0100

    Create join.tsx

---
+++ prodev-mobile-app-0x04/app/join.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function JoinScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Join Screen</Text>
+    </View>
+  );
+}
