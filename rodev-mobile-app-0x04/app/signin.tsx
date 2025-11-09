55a5aa9b63e7c1e7a323c104d0f9e86619af2bf3
Amarire Dev <108834223+Said-Amarire@users.noreply.github.com>
2025-11-09 20:34:28 +0100

    Delete rodev-mobile-app-0x04 directory

--- rodev-mobile-app-0x04/app/signin.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function SigninScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Sign In Screen</Text>
-    </View>
-  );
-}
